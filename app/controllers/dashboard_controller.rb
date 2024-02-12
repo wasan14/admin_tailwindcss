5b24eaf62523ba5ab70fb3458b054b4d4bfa4332
Rohit Kumar <wasanrohit14@gmail.com>
2024-02-13 00:01:11 +0530

    Admin panel

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,18 @@
+class DashboardController < ApplicationController
+
+	def dashboard
+	end
+
+	def all_professor
+	end
+
+	def add_professor
+	end
+
+	def edit_professor
+	end
+
+	def about_professor
+	end
+
+end
